d7b048371ef4841d4abe741eaee5c42f406fcb24
Ronai Melo <amzronai@gmail.com>
2021-11-25 23:27:03 -0300

    Nova atualizaçao de front-end

--- feirasjc/feirapage/static/feirapage/script.js
+++ feirasjc/feirapage/static/feirapage/script.js
@@ -1,17 +1,3 @@
-const header = document.querySelector('#header');
-const nav = header.offsetHeight;
-
-window.addEventListener('scroll', function() {
-    if(window.scrollY >= nav) {
-
-        header.classList.add('scroll')
-
-    } else {
-        header.classList.remove('scroll')
-
-    }
-})
-
 const swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
     pagination: {
@@ -35,7 +21,42 @@ sr.reveal(
 #about img, #about .text,
 #services img, #services .text,
 #testimonials img, #testimonials .text,
-#contact img, #contact .text`
+#contact img, #contact .text,
+footer .brand, footer .social`
 , { interval: 100});
+
+/* ALTERA O HEADER QUANDO SCROLL */
+function header() {
+    const header = document.querySelector('#header');
+    const nav = header.offsetHeight;
+    if(window.scrollY >= nav) {
+
+        header.classList.add('scroll')
+
+    } else {
+        header.classList.remove('scroll')
+
+    }
+}
+
+/* Voltar ao topo*/
+function voltarAoTopo () {
+    const voltarAoTopo = document.querySelector('.topo')
+    if(window.scrollY >= 560) {
+        voltarAoTopo.classList.add('show')
+    } else {
+        voltarAoTopo.classList.remove('show')
+    }
+}
+
+/*EXECUTA FUNÇÕES QUANDO SCROLL*/
+window.addEventListenet('scroll', function() {
+    header()
+    voltarAoTopo()
+})
+
+
+
+
 
 
